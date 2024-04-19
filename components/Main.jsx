1742e12f9fb13224df166e9de3268c7e85ff4939
Jassi2004 <bedijaskirat2004@gmail.com>
2024-04-19 09:24:58 +0530

    third message

--- components/Main.jsx
+++ components/Main.jsx
@@ -23,13 +23,13 @@ export default function Main(props){
     return (
         <>
 
-            <div className="w-96 h-3/4 bg-blue-200 rounded-xl border-solid border-2 border-black p-5 overflow-hidden">
+            <div className="w-96 h-3/4  bg-orange-300 rounded-xl border-solid border-2 border-black p-5 overflow-hidden">
 
                 <div className=" w-full h-14 backdrop-blur-sm bg-amber-400 rounded-xl flex justify-evenly items-center">
 
                     <input value={task} onChange={handleChange} type="text" placeholder="Add" class="w-3/4 py-2 px-4 rounded-md border border-black focus:outline-none focus:border-blue-500 bg-amber-400 " />
 
-                    <button onClick={addTaskButton} className="bg-blue-200 hover:bg-blue-500 text-black hover:text-white font-bold py-4 px-4 rounded-full flex items-center space-x-2">
+                    <button onClick={addTaskButton} className=" bg-orange-300 hover:bg-blue-500 text-black hover:text-white font-bold py-4 px-4 rounded-full flex items-center space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                           <path fillRule="evenodd" d="M10 2a1 1 0 0 1 1 1v6h6a1 1 0 0 1 0 2h-6v6a1 1 0 1 1-2 0v-6H3a1 1 0 1 1 0-2h6V3a1 1 0 0 1 1-1z" clipRule="evenodd" />
                         </svg>
